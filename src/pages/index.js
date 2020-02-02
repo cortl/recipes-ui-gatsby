ebe1dc4cac0fe3d6870cf2e0c91e47e7b54ba2dc
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2020-02-02 11:00:18 -0600

    adding infinite scroll

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {useStaticQuery, graphql} from 'gatsby';
 
 import Layout from '../components/layout';
@@ -7,9 +7,25 @@ import {Search} from '../components/search';
 import {Feature} from '../components/feature';
 
 const IndexPage = () => {
+	const [shown, setShown] = useState(6);
+	const handleScroll = e => {
+		const scrolled = Math.round(
+			window.innerHeight + document.documentElement.scrollTop
+		);
+		const totalScrollable = document.documentElement.offsetHeight;
+		if (scrolled === totalScrollable) {
+			setShown(prev => prev + 6);
+		}
+	};
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	});
 	const data = useStaticQuery(graphql`
 		query TopRecipesQuery {
-			allRecipesJson(limit: 9, sort: {fields: rating, order: DESC}) {
+			allRecipesJson(sort: {fields: rating, order: DESC}) {
 				nodes {
 					slug
 					title
@@ -29,12 +45,11 @@ const IndexPage = () => {
 	return (
 		<Layout>
 			<SEO title='Home' />
-
 			<div className='mw100 mw9-m mw8-l center'>
 				<Search />
 				<div>
-					{data.allRecipesJson.nodes.map((recipe, i) => (
-						<Feature {...recipe} />
+					{data.allRecipesJson.nodes.slice(0, shown).map((recipe, i) => (
+						<Feature key={`recipe${i}`} {...recipe} />
 					))}
 				</div>
 			</div>
