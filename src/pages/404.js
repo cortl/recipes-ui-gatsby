ebc92cd696b8bac69d0f13366c69c181735dd57b
cortl <cortlan.bainbridge@gmail.com>
2020-09-27 17:20:40 -0500

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,9 +5,11 @@ import SEO from '../components/seo';
 
 const NotFoundPage = () => (
 	<Layout>
-		<SEO title='404: Not found' />
-		<h1>NOT FOUND</h1>
-		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+		<SEO title='404' />
+		<div className='w-100 center tc'>
+			<h1 className='f2'>{'Oops! 😿'}</h1>
+			<p className='f3'>{`Looks like you've taken a wrong turn!`}</p>
+		</div>
 	</Layout>
 );
 
